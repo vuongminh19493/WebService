91cc7e348e08c4d442e7ed6146fd4d98fb3f9a93
kylix.par <minhvcth1806008@fpt.edu.vn>
2019-12-13 20:43:58 +0700

    Webservice

---
+++ DemoRESTServiceCRUD/Book.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DemoRESTServiceCRUD
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember] public int BookId { get; set; }
+        [DataMember] public string Title { get; set; }
+        [DataMember] public string ISBN { get; set; }
+        public interface IBookRespository
+        {
+            List<Book> GetAllBooks();
+            Book GetBookById(int id);
+            Book AddNewBook(Book item);
+            bool DeleteABook(int id);
+            bool UpdateABook(Book item);
+        }
+        public class BookRespository: IBookRespository
+        {
+            private List<Book> books = new List<Book>();
+            private int counter = 1;
+
+            public BookRespository()
+            {
+                AddNewBook(new Book{Title = "C# Programming", ISBN = "23422342343"});
+                AddNewBook(new Book { Title = "Java Programming", ISBN = "123422342343" });
+                AddNewBook(new Book { Title = "WCF Programming", ISBN = "231324342343" });
+            }
+            //CRUD Operations
+            //1. CREATE
+            public Book AddNewBook(Book newBook)
+            {
+                if (newBook == null)
+                    throw new ArgumentNullException("newBook");
+                newBook.BookId = counter++;
+                books.Add(newBook);
+                return newBook;
+            }
+            //2. RETRIEVE /ALL
+            public List<Book> GetAllBooks()
+            {
+                return books;
+            }
+            //3. RETRIEVE /By BookId
+            public Book GetBookById(int bookId)
+            {
+                return books.Find(b => b.BookId == bookId);
+            }
+            //4. UPDATE
+            public bool UpdateABook(Book updatedBook)
+            {
+                if (updatedBook == null)
+                    throw new ArgumentNullException("updatedBook");
+                int idx = books.FindIndex(b => b.BookId == updatedBook.BookId);
+                if (idx == -1)
+                    return false;
+                books.RemoveAt(idx);
+                books.Add(updatedBook);
+                return true;
+            }
+            //5. DELETE
+            public bool DeleteABook(int bookId)
+            {
+                int idx = books.FindIndex(b => b.BookId == bookId);
+                if (idx == -1)
+                    return false;
+                books.RemoveAll(b => b.BookId == bookId);
+                return true;
+            }
+        }
+    }
+}
---
+++ DemoRESTServiceCRUD/BookServices.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DemoRESTServiceCRUD
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookServices" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookServices.svc or BookServices.svc.cs at the Solution Explorer and start debugging.
+    public class BookServices : IBookServices
+    {
+        static  Book.IBookRespository respository = new Book.BookRespository();
+
+        public List<Book> GetBookList()
+        {
+            return respository.GetAllBooks();
+        }
+
+        public Book GetBookById(string id)
+        {
+            return respository.GetBookById(int.Parse(id));
+        }
+
+        public string AddBook(Book book, string id)
+        {
+            Book newBook = respository.AddNewBook(book);
+            return "id=" + newBook.BookId;
+        }
+
+        public string UpdateBook(Book book, string id)
+        {
+            bool updated = respository.UpdateABook(book);
+            if (updated)
+                return "Book with id = " + id + "updated successfully.";
+            else
+                return "Unable to update book with id = " + id;
+        }
+
+        public string DeleteBook(string id)
+        {
+            bool deleted = respository.DeleteABook(int.Parse(id));
+            if (deleted)
+                return "Book with id = " + id + "deleted successfully.";
+            else return "Unable to delete book with id = " + id;
+        }
+        public void DoWork()
+        {
+        }
+    }
+}
